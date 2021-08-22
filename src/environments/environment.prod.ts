7f30a93d1ba71c1e89393231e20215a7528a60be
Matimba Moses <matimba.moses@boxfusion.io>
2021-08-23 00:04:34 +0200

    Added Packages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyAqWI8o1EKoMb9RsxPCUwMIdlgvrQUghSw",
+    authDomain: "oshop-747e0.firebaseapp.com",
+    projectId: "oshop-747e0",
+    storageBucket: "oshop-747e0.appspot.com",
+    messagingSenderId: "719369750644",
+    appId: "1:719369750644:web:55eeac61b454c08e1ba921",
+    measurementId: "G-VJPJ6Y8C0J"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyAqWI8o1EKoMb9RsxPCUwMIdlgvrQUghSw",
+    authDomain: "oshop-747e0.firebaseapp.com",
+    projectId: "oshop-747e0",
+    storageBucket: "oshop-747e0.appspot.com",
+    messagingSenderId: "719369750644",
+    appId: "1:719369750644:web:55eeac61b454c08e1ba921",
+    measurementId: "G-VJPJ6Y8C0J"
+  }
 };
 
 /*
